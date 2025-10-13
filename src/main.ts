e6ae15bd9ef484caeb5ebe24344d78d878ff8d89
aa7355608 <lliu0121@student.monash.edu>
2025-10-13 16:31:48 +1100

    update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
-import '@/assets/styles.css'
+import "./styles.css"
 import { useAuthStore } from '@/store/auth'
 
 const app = createApp(App)
